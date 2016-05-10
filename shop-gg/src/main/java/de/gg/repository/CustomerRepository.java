cd840d994a4faf88f6c5cd588e65925819c6e73d
praktika <marcel.florian@student.fhws.de>
2016-05-10 17:27:46 +0200

    clean up

--- shop-gg/src/main/java/de/gg/repository/CustomerRepository.java
+++ shop-gg/src/main/java/de/gg/repository/CustomerRepository.java
@@ -2,7 +2,6 @@ package de.gg.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.Repository;
 import de.gg.model.Customer;
 
