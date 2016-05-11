90d6aab899d0875a0adc8376d5c00370574094e7
praktika <marcel.florian@student.fhws.de>
2016-05-11 15:24:17 +0200

    angular material

--- shop-gg/src/main/java/de/gg/repository/CustomerRepository.java
+++ shop-gg/src/main/java/de/gg/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package de.gg.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
@@ -8,4 +10,5 @@ import de.gg.model.Customer;
 public interface CustomerRepository extends Repository<Customer, String> {
 
 	Page<Customer> findAll(Pageable pageable);
+
 }
--- shop-gg/src/main/resources/static/js/app.js
+++ shop-gg/src/main/resources/static/js/app.js
@@ -1,19 +1,175 @@
-var app = angular.module('GgApp', ['ngMaterial'])
+var app = angular.module('GgApp', ['ngMaterial','ngResource','md.data.table','spring-data-rest'])
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $http, SpringDataRestAdapter) {
 
 })
 
 .controller('ToolbarCtrl', function($scope) {
-    var iconData = [
-          {name: 'icon-user-plus'   , color: "rgb(89, 226, 168)" },
-          {name: 'icon-settings'    , color:"#A00", theme:"md-warn md-hue-5"}
-        ];
-    $scope.sizes = [
-      {size:48,padding:10},
-      {size:36,padding:6},
-      {size:24,padding:2},
-      {size:12,padding:0}
-    ];
-    $scope.fonts = [].concat(iconData);
-});
+
+})
+
+.controller('nutritionController', ['$http', '$mdEditDialog', '$q', '$timeout', '$scope', 'SpringDataRestAdapter', function ($http, $mdEditDialog, $q, $timeout, $scope, SpringDataRestAdapter) {
+  'use strict';
+
+  $scope.options = {
+    rowSelection: true,
+    multiSelect: true,
+    autoSelect: true,
+    decapitate: false,
+    largeEditDialog: false,
+    boundaryLinks: false,
+    limitSelect: true,
+    pageSelect: true
+  };
+
+  $scope.selected = [];
+  $scope.limitOptions = [5, 10, 15, {
+    label: 'All',
+    value: function () {
+      return $scope.customers ? $scope.customers.count : 0;
+    }
+  }];
+
+  $scope.query = {
+    order: 'name',
+    limit: 5,
+    page: 1
+  };
+
+  // for testing ngRepeat
+  $scope.columns = [{
+    name: 'Dessert',
+    orderBy: 'name',
+    unit: '100g serving'
+  }, {
+    descendFirst: true,
+    name: 'Type',
+    orderBy: 'type'
+  }, {
+    name: 'Calories',
+    numeric: true,
+    orderBy: 'calories.value'
+  }, {
+    name: 'Fat',
+    numeric: true,
+    orderBy: 'fat.value',
+    unit: 'g'
+  }, /* {
+    name: 'Carbs',
+    numeric: true,
+    orderBy: 'carbs.value',
+    unit: 'g'
+  }, */ {
+    name: 'Protein',
+    numeric: true,
+    orderBy: 'protein.value',
+    trim: true,
+    unit: 'g'
+  }, /* {
+    name: 'Sodium',
+    numeric: true,
+    orderBy: 'sodium.value',
+    unit: 'mg'
+  }, {
+    name: 'Calcium',
+    numeric: true,
+    orderBy: 'calcium.value',
+    unit: '%'
+  }, */ {
+    name: 'Iron',
+    numeric: true,
+    orderBy: 'iron.value',
+    unit: '%'
+  }, {
+    name: 'Comments',
+    orderBy: 'comment'
+  }];
+  
+  $scope.customers = []
+
+  var httpPromise = $http.get('http://localhost:8080/customers').success(function (response) {
+      $scope.response = angular.toJson(response, true);
+  });
+
+  SpringDataRestAdapter.process(httpPromise, 'parent', true).then(function (processedResponse) {
+      $scope.processedResponse = angular.toJson(processedResponse, true)._embedded;
+      //$scope.customers = processedResponse._embeddedItems;
+      
+	  angular.forEach(processedResponse._embeddedItems, function (customer, key) {
+		    console.log("Customer name: " + customer.nachname);
+		    $scope.customers.push(customer);
+	  });
+  });
+
+  $scope.editComment = function (event, dessert) {
+    event.stopPropagation();
+
+    var dialog = {
+      // messages: {
+      //   test: 'I don\'t like tests!'
+      // },
+      modelValue: dessert.comment,
+      placeholder: 'Add a comment',
+      save: function (input) {
+        dessert.comment = input.$modelValue;
+      },
+      targetEvent: event,
+      title: 'Add a comment',
+      validators: {
+        'md-maxlength': 30
+      }
+    };
+
+    var promise = $scope.options.largeEditDialog ? $mdEditDialog.large(dialog) : $mdEditDialog.small(dialog);
+
+    promise.then(function (ctrl) {
+      var input = ctrl.getInput();
+
+      input.$viewChangeListeners.push(function () {
+        input.$setValidity('test', input.$modelValue !== 'test');
+      });
+    });
+  };
+
+  $scope.toggleLimitOptions = function () {
+    $scope.limitOptions = $scope.limitOptions ? undefined : [5, 10, 15];
+  };
+
+  $scope.getTypes = function () {
+    return ['Candy', 'Ice cream', 'Other', 'Pastry'];
+  };
+
+  $scope.onPaginate = function(page, limit) {
+    console.log('Scope Page: ' + $scope.query.page + ' Scope Limit: ' + $scope.query.limit);
+    console.log('Page: ' + page + ' Limit: ' + limit);
+
+    $scope.promise = $timeout(function () {
+
+    }, 2000);
+  };
+
+  $scope.deselect = function (item) {
+    console.log(item.name, 'was deselected');
+  };
+
+  $scope.log = function (item) {
+    console.log(item.name, 'was selected');
+  };
+
+  $scope.loadStuff = function () {
+    $scope.promise = $timeout(function () {
+
+    }, 2000);
+  };
+
+  $scope.onReorder = function(order) {
+
+    console.log('Scope Order: ' + $scope.query.order);
+    console.log('Order: ' + order);
+
+    $scope.promise = $timeout(function () {
+
+    }, 2000);
+  };
+
+}]);
